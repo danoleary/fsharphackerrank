dfbb7a5303bd7e6330048fb5fafced57b22a210d
Daniel O'Leary <daniel@liveposter.com>
2016-08-18 17:02:38 +0100

    add binary numbers

--- 10_BinaryNumbers.fs
+++ 10_BinaryNumbers.fs
@@ -4,15 +4,37 @@ open System
 
 module BinaryNumbers =
 
-    let n = 5
+    let decimalN = int (Console.ReadLine())
 
-    //convert n to binary
+    let rec decimalToBinary n acc = 
+        match n with
+        | 0 -> acc
+        | _ -> decimalToBinary (n / 2) (string (n % 2) + acc)
 
-    //find base-10 integer denoting the maximum number of consecutive 1's in n's binary representation
+    let binaryN = decimalToBinary decimalN ""
 
+    let binaryLength = String.length binaryN
 
+    let maxConsecutiveOnesFromStart = [0..(binaryLength - 1)] 
+                                    |> List.map binaryN.Substring 
+                                    |> List.filter (fun x -> (x.Contains "0") = false) 
+                                    |> List.maxBy String.length 
+                                    |> String.length
 
-      
+    let maxConsecutiveOnesFromEnd = [(binaryLength - 1)..0] 
+                                    |> List.map binaryN.Substring 
+                                    |> List.filter (fun x -> (x.Contains "0") = false) 
+                                    |> List.maxBy String.length 
+                                    |> String.length
+
+    let maxInt x y =
+            match x with
+            | x when x > y -> x
+            | _ -> y
+
+    let max = maxInt maxConsecutiveOnesFromStart maxConsecutiveOnesFromEnd
+
+    printfn "%i" max
  
 
 
--- 3_IntroToConditionalStatements.fs
+++ 3_IntroToConditionalStatements.fs
@@ -4,17 +4,14 @@ open System
 
 module IntroToConditionalStatements =
 
-    let isEven x = (x % 2) = 0
-    let isOdd x = isEven x = false
-
-    let printValue x= 
+    let printValue x = 
         match x with
-        | x when isOdd x -> printfn "Weird"
-        | x when isEven x ->
+        | x when (x % 2) = 0 ->
             match x with
             | x when x >= 2 && x <= 5 -> printfn "Not Weird"
             | x when  x >= 6 && x <= 20 -> printfn "Weird"
             | _ -> printfn "Not Weird"
+        | _  -> printfn "Weird"
 
     let input =  Console.ReadLine()
     
